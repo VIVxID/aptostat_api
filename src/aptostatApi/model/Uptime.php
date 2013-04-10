0b4503a3d1f7e85c4837cdb064fe74d6b337475c
Ketil Ovrebo <ketilovre@gmail.com>
2013-04-10 10:41:31 +0200

    Moved Live and Uptime down into gather, the API functions now only use the cache to fetch information

--- src/aptostatApi/model/Uptime.php
+++ src/aptostatApi/model/Uptime.php
@@ -5,17 +5,6 @@ namespace aptostatApi\model;
 class Uptime
 {
     private $feed = array();
-
-    //Hostnames and their corresponding Pingdom ID's
-    private $hosts = array(
-            "Atika Backoffice" => 615766,
-            "DrVideo Encoding" => 615772,
-            "DrFront Backoffice" => 615760,
-            "DrVideo Backoffice" => 615764,
-            "DrVideo CDN" => 615768,
-            "DrVideo API" => 615770,
-            "DrPublish Backoffice" => 615767,
-            "DrPublish API" => 615771);
     
     public function __construct()
     {
@@ -24,47 +13,11 @@ class Uptime
 
     public function query()
     {
-        $login = file('/var/apto/ping', FILE_IGNORE_NEW_LINES);
-        $curl = curl_init();
-        $out = array();
+
         $m = new \Memcached();
         $m->addServer("localhost",11211);
 
         $out = $m->get("uptime");
-
-        if ($out === false) {
-
-            //Gets uptime history for the last week for every service.
-            foreach ($this->hosts as $hostName => $hostID) {
-
-                $out[$hostName] = array();
-
-                $options = array(
-                    CURLOPT_URL => "https://api.pingdom.com/api/2.0/summary.outage/$hostID",
-                    CURLOPT_CUSTOMREQUEST => "GET",
-                    CURLOPT_USERPWD => $login[0].":".$login[1],
-                    CURLOPT_HTTPHEADER => array("App-Key: ".$login[2]),
-                    CURLOPT_RETURNTRANSFER => true
-                    );
-            
-                // Execute 
-                curl_setopt_array($curl,$options);
-                $response = json_decode(curl_exec($curl),true);
-                $checkList = $response["summary"]["states"];
-                
-                foreach ($checkList as $check) {
-                
-                    if ($check["status"] != "up") {
-             
-                        $out[$hostName][$check["timefrom"]] = $check["status"];
-                    
-                    }
-                }
-            }
-            
-            $m->set("uptime", $out, 43200);
-            
-        }
         
         if (empty($out)) {
             return 500; // Internal Error
