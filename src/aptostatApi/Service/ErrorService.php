930f30ee8cb6f8ccbea84a5b7b9ba02688e313eb
Noha Xue <noxu27@gmail.com>
2013-05-02 13:05:16 +0200

    Try to get errorhandling working

--- src/aptostatApi/Service/ErrorService.php
+++ src/aptostatApi/Service/ErrorService.php
@@ -14,7 +14,7 @@ class ErrorService
     {
         $code = $e->getCode();
         $formattedErrorMsg['error']['statusCode'] = $code;
-        if ($code != 0) {
+        if ($code != 0 or $code != '0') {
             $formattedErrorMsg['error']['statusDesc'] = \Symfony\Component\HttpFoundation\Response::$statusTexts[$code];
         }
         $formattedErrorMsg['error']['errorMessage'] = $e->getMessage();
