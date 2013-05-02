50347b7e56090f6113269b9d3836316f77a67dbc
Noha Xue <noxu27@gmail.com>
2013-05-02 13:17:25 +0200

    Variable paths

--- src/aptostatApi/Service/KillswitchService.php
+++ src/aptostatApi/Service/KillswitchService.php
@@ -5,9 +5,16 @@ namespace aptostatApi\Service;
 
 class KillswitchService
 {
+    private $gatherKillPath;
+
+    public function __construct()
+    {
+        $this->gatherKillPath = __DIR__ . '../../../app/lock/gatherKill.lock';
+    }
+
     public function getSwitchStatus()
     {
-        if (file_exists('../../../app/lock/gatherKill.lock')) {
+        if (file_exists($this->gatherKillPath)) {
             return array('killswitchStatus' => 'on');
         } else {
             return array('killswitchStatus' => 'off');
@@ -16,15 +23,15 @@ class KillswitchService
 
     public function killSystem()
     {
-        if (file_exists('../../../app/lock/gatherKill.lock')) {
+        if (file_exists($this->gatherKillPath)) {
             return array(
                 'killswitchStatus' => 'on',
                 'message' => 'The system has already been stopped from fetching new reports'
             );
         } else {
-            touch("../../../app/lock/gatherKill.lock");
+            touch($this->gatherKillPath);
 
-            if (!file_exists('../../../app/lock/gatherKill.lock')) {
+            if (!file_exists($this->gatherKillPath)) {
                 throw new \Exception('Could not shut down the system. Contact admin.', 500);
             }
 
@@ -37,15 +44,15 @@ class KillswitchService
 
     public function reviveSystem()
     {
-        if (!file_exists('../../../app/lock/gatherKill.lock')) {
+        if (!file_exists($this->gatherKillPath)) {
             return array(
                 'killswitchStatus' => 'off',
                 'message' => 'The system is already running'
             );
         } else {
-            delete("../../../app/lock/gatherKill.lock");
+            delete($this->gatherKillPath);
 
-            if (file_exists('gatherKill.lock')) {
+            if (file_exists($this->gatherKillPath)) {
                 throw new \Exception('Could not turn on the system. Contact admin.', 500);
             }
 
